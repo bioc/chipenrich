2bd7778dec0429f8b267eb73cfda12038e7af123
Wang <wangdaha@med.umich.edu>
2025-03-11 15:50:54 -0400

    fix test bug

--- R/chipenrich.R
+++ R/chipenrich.R
@@ -273,10 +273,10 @@ chipenrich = function(
 	ppg = num_peaks_per_gene(assigned_peaks, ldef, mappa)
 
 	# Add relevant columns to ppg depending on the method
-	if(method == 'chipapprox_old') {
-		message("Calculating weights for approximate method..")
-		ppg = calc_approx_weights(ppg,mappa)
-	}
+	#if(method == 'chipapprox_old') {
+	#	message("Calculating weights for approximate method..")
+	#	ppg = calc_approx_weights(ppg,mappa)
+	#}
 
 	######################################################
 	# Enrichment
